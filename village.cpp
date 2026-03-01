b4641d11cfb535dc7ba98cd1ebf0531dc20019cb
ElementaleLord <patrickfarah200515@outlook.com>
2026-03-01 13:32:50 +0200

    Cleaned up uneeded parts

--- village.cpp
+++ village.cpp
@@ -28,52 +28,57 @@ void drawHouse(point peak, GLfloat width, GLfloat height, color c){
     // house color
     glColor3f(c.r, c.g, c.b);
     
+    glBegin(GL_LINE_LOOP);
     //~ house base
-    // bottom line of house square
-    glVertex2f(peak.x, peak.y);
-    glVertex2f(peak.x+ width, peak.y);
     // right line of house square
-    glVertex2f(peak.x+ width, peak.y);
     glVertex2f(peak.x+ width, peak.y+ height);
+    // bottom line of house square
+    glVertex2f(peak.x+ width, peak.y);
+    glVertex2f(peak.x, peak.y);
     // left line of house square
     glVertex2f(peak.x, peak.y+ height);
-    glVertex2f(peak.x, peak.y);
+    
+    glEnd();
 
+    glBegin(GL_LINE_LOOP);
     //~ roof
+    // right side of roof triangle
+    glVertex2f(peak.x+ width,   peak.y+ height);
     // top line of house square / bottom line of roof triangle
     glVertex2f(peak.x+ width, peak.y+ height);
     glVertex2f(peak.x, peak.y+ height);
     // left side of roof triangle
-    glVertex2f(peak.x, peak.y+ height);
     glVertex2d(peak.x+ width* 0.5, peak.y+ height* 1.7);
-    // right side of roof triangle
-    glVertex2f(peak.x+ width,   peak.y+ height);
-    glVertex2d(peak.x+ width* 0.5, peak.y+ height* 1.7);
+    
+    glEnd();
 
+    glBegin(GL_LINE_LOOP);
     //~door
-    // left side of door
-    glVertex2f(peak.x+ width*0.2, peak.y);
-    glVertex2f(peak.x+ width*0.2, peak.y+ height* 0.5);
     // right side of door
     glVertex2f(peak.x+ width*0.4, peak.y);
-    glVertex2f(peak.x+ width*0.4, peak.y+ height* 0.5);
     // top of door
-    glVertex2f(peak.x+ width*0.2, peak.y+ height* 0.5);
     glVertex2f(peak.x+ width*0.4, peak.y+ height* 0.5);
+    glVertex2f(peak.x+ width*0.2, peak.y+ height* 0.5);
+    // left side of door
+    glVertex2f(peak.x+ width*0.2, peak.y);
 
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
     //~ window
-    // bottom side of window
-    glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.25);
-    glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.25);
-    // left side of window
-    glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.25);
-    glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.5);
     // right side of window
-    glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.25);
     glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.5);
+    // bottom side of window
+    glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.25);
+    glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.25);
+    // left side of window
+    glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.5);
     // top side of window
     glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.5);
-    glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.5);
+
+    glEnd();
+
+    glBegin(GL_LINES);
     // horizontal bar of window
     glVertex2f(peak.x+ width*0.6, peak.y+ height* 0.375);
     glVertex2f(peak.x+ width*0.8, peak.y+ height* 0.375);
@@ -81,30 +86,33 @@ void drawHouse(point peak, GLfloat width, GLfloat height, color c){
     glVertex2f(peak.x+ width*0.7, peak.y+ height* 0.5);
     glVertex2f(peak.x+ width*0.7, peak.y+ height* 0.25);
 
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
     //~ Chimney
-    // left side  of chimney
-    glVertex2f(peak.x+ width*0.7, peak.y+ height* 1.42);
-    glVertex2f(peak.x+ width*0.7, peak.y+ height* 1.6);
     // right side of chimney
     glVertex2f(peak.x+ width*0.9, peak.y+ height* 1.14);
     glVertex2f(peak.x+ width*0.9, peak.y+ height* 1.6);
-    // top of chimney
-    glVertex2f(peak.x+ width*0.65, peak.y+ height* 1.6);
-    glVertex2f(peak.x+ width*0.95, peak.y+ height* 1.6);
-    // left side of top of chimney
-    glVertex2f(peak.x+ width*0.65, peak.y+ height* 1.6);
-    glVertex2f(peak.x+ width*0.65, peak.y+ height* 1.65);
+    // left side  of chimney
+    glVertex2f(peak.x+ width*0.7, peak.y+ height* 1.6);
+    glVertex2f(peak.x+ width*0.7, peak.y+ height* 1.42);
+
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
     // right side of top of chimney
     glVertex2f(peak.x+ width*0.95, peak.y+ height* 1.6);
     glVertex2f(peak.x+ width*0.95, peak.y+ height* 1.65);
     // true top of chimney
     glVertex2f(peak.x+ width*0.65, peak.y+ height* 1.65);
-    glVertex2f(peak.x+ width*0.95, peak.y+ height* 1.65);
+    // left side of top of chimney
+    glVertex2f(peak.x+ width*0.65, peak.y+ height* 1.6);
+
+    glEnd();
 }
 
 void drawVillage(void){
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_LINES);
 
     drawHouse({ 70, 210},    5,   5, colorMap.at("White"));    // very far white house
     drawHouse({170, 210},   10,  10, colorMap.at("Purple"));   // far-far purple house
@@ -114,7 +122,6 @@ void drawVillage(void){
     drawHouse({140,  50},   80,  80, colorMap.at("Cyan"));     // close-middle cyan house
     drawHouse({ 10,  10},  100, 100, colorMap.at("Blue"));     // close blue house
 
-    glEnd();
     glFlush();
 }
 
